e68973850eab7f7d84db52e93e372ee8209d9eb2
Panu Boon <thevisiblehands@gmail.com>
2013-07-08 18:01:33 +0000

    Changed web.js to read from file

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
 
+var buffer= new Buffer(64);
+
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+  buffer.write(data);
+});
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString("utf-8");
 });
 
 var port = process.env.PORT || 5000;
